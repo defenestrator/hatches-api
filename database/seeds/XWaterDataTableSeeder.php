1998bd8cf16000616fa5a03af96c77afdb92cabb
defenestrator <jeremyblc@gmail.com>
2015-02-23 21:39:55 -0700

    Blachchachhkkhkhkchhk! bugs!

---
+++ database/seeds/XWaterDataTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Hatches\WaterData;
+use Faker\Factory as Faker;
+
+class WaterDataTableSeeder extends DatabaseSeeder {
+
+	public function run()
+	{
+		$faker = Faker::create();
+
+		foreach(range(1, 10) as $index)
+		{
+			WaterData::create([
+            'date' => $faker->dateTimeThisYear(),
+            'lat'   => $faker->latitude,
+            'lng'   => $faker->longitude,
+            'water_data' => $faker->paragraph()
+			]);
+		}
+	}
+
+}
