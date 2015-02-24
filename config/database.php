e818ecab1b19b1c61e904b42e227a35954ad3709
defenestrator <jeremyblc@gmail.com>
2015-02-23 21:31:25 -0700

    travis mysql tweaks.

--- config/database.php
+++ config/database.php
@@ -54,9 +54,9 @@ return [
 
 		'mysql' => [
 			'driver'    => 'mysql',
-			'host'      => env('DB_HOST', 'localhost'),
-			'database'  => env('DB_DATABASE', 'forge'),
-			'username'  => env('DB_USERNAME', 'forge'),
+			'host'      => env('DB_HOST', '127.0.0.1'),
+			'database'  => env('DB_DATABASE', 'travis'),
+			'username'  => env('DB_USERNAME', 'travis'),
 			'password'  => env('DB_PASSWORD', ''),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
