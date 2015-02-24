96e096c69df1bdae6fda1d3d3919f1541ac722f7
defenestrator <jeremyblc@gmail.com>
2015-02-23 21:21:38 -0700

    This is not BDD! Soon it will be.

--- spec/BackupWaterDataSpec.php
+++ spec/BackupWaterDataSpec.php
@@ -5,10 +5,12 @@ namespace spec\Hatches;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
-class WaterdataSpec extends ObjectBehavior
+class WaterDataSpec extends ObjectBehavior
 {
     function it_is_initializable()
     {
-        $this->shouldHaveType('Hatches\Waterdata');
+        $this->shouldHaveType('Hatches\WaterData');
     }
+
+
 }
