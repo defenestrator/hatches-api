19a93e9903434cc82081f538be579f4f0bbc9e08
defenestrator <jeremyblc@gmail.com>
2015-02-26 10:56:50 -0700

    fix user_buddy constraint violation

--- database/migrations/2014_05_25_025904_create_empty_tables.php
+++ database/migrations/2014_05_25_025904_create_empty_tables.php
@@ -38,9 +38,9 @@ class CreateEmptyTables extends Migration
     public function up()
     {
         // using sqlite for test env portability
-//        if (app()->environment() !== 'acceptance' || 'testing') {
-//            DB::statement('SET FOREIGN_KEY_CHECKS=0');
-//        }
+        if (app()->environment() !== 'acceptance' || 'testing') {
+            DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        }
 
         Schema::create('roles', function ($table) {
             // In descending order of permissions
@@ -552,9 +552,9 @@ class CreateEmptyTables extends Migration
         foreach ($this->tables as $tableName) {
             Schema::drop($tableName);
         }
-//        if (app()->environment() !== 'acceptance' || 'testing') {
-//            DB::statement('SET FOREIGN_KEY_CHECKS=1');
-//        }
+        if (app()->environment() !== 'acceptance' || 'testing') {
+            DB::statement('SET FOREIGN_KEY_CHECKS=1');
+        }
 
     }
 
