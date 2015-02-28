1de7d274da81f27c2d658cb2181bfc11e34aee89
defenestrator <jeremyblc@gmail.com>
2015-02-27 22:20:35 -0700

    nyan formatters, gulp sans elixir, etc.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
 "use strict"
-
+var gulp = require('gulp');
+var behat = require('gulp-behat');
+var phpUnit = require('gulp-phpunit');
+var phpSpec = require('gulp-phpspec');
 var elixir = require('laravel-elixir');
 require('laravel-elixir-behat');
+
 
 /*
  |--------------------------------------------------------------------------
@@ -14,18 +18,25 @@ require('laravel-elixir-behat');
  |
  */
 
+//elixir(function (mix) {
+//    mix.behat();
+//    mix.phpUnit();
+//    mix.phpSpec();
+//});
 
 
-elixir(function (mix) {
-    mix.behat();
-    mix.phpUnit();
-    mix.phpSpec();
+gulp.task('behat', function() {
+    var options = {debug: false};
+    gulp.src('./features/**/*.php').pipe(behat('./vendor/bin/behat',options));
 });
 
+gulp.task('phpSpec', function() {
+    var opt = {runTests: 'run'};
+    gulp.src('./tests/**/*.php').pipe(phpSpec('./vendor/bin/phpspec run -v',opt));
+});
+gulp.task('phpUnit', function() {
+    var options = {run: 'run', switch: '-v'};
+    gulp.src('./spec/**/*.php').pipe(phpUnit('./vendor/bin/phpunit',options));
+});
 
-//gulp.task('behat', function() {
-//    var options = {debug: false};
-//    gulp.src('./features/**/*.php').pipe(behat('./vendor/bin/behat',options));
-//});
-//
-//gulp.task('default', ['behat', 'elixir']);
+gulp.task('default', ['behat', 'phpUnit', 'phpSpec']);
