7a8b0a5623905b7bf81ad906a8f2e2847d67a4cc
Jeremy <jeremy@edos.io>
2015-02-28 21:34:55 -0700

    configs for real life

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -48,6 +48,7 @@
                     <li><a href="/auth/login">Login</a></li>
                     <li><a href="/auth/register">Register</a></li>
                 @else
+                    <li><a href="/docs">Docs</a></li>
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
                         <ul class="dropdown-menu" role="menu">
