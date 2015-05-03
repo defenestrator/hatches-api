f0f07543c612a824a7e1c1766a12ff10ec018b1d
Jeremy <jeremy@edos.io>
2015-05-03 10:42:20 -0600

    fix that junk, dang

--- resources/scripts/post-commit-process.sh
+++ resources/scripts/post-commit-process.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# word, to your mother, even.
-cd /var/www/hatches-api/
+git clone https://github.com/defenestrator/hatches-api.git && cd /var/www/hatches-api/
 # probably not needed
 #npm install -g n
 #npm n stable
@@ -12,13 +11,13 @@ chmod chgroup -R www-data /var/www/hatches-api/
 # rm /etc/nginx/sites-available/default;
 # mv nginx.default /etc/nginx/sites-available/default;
 service nginx restart;
-mv .env.example .env && mv .env.behat.travis .env.behat
+mv .env.behat.travis .env.behat && mv .env.blank .env
 touch /var/www/hatches-api/storage/database.sqlite /var/www/hatches-api/storage/acceptance/acceptance.sqlite
 chmod -R 0775 /var/www/hatches-api/storage && chmod 0777 /var/www/hatches-api/storage/database.sqlite /var/www/hatches-api/storage/acceptance/acceptance.sqlite
 npm install -g gulp
-php artisan flyfish
 php artisan key:generate
+php artisan flyfish
 php artisan migrate --seed --force #will be remved in realsies production env
 php artisan vendor:publish
-/var/www/hatches-api/vendor/bin/behat --init
+./vendor/bin/behat --init
 gulp test
