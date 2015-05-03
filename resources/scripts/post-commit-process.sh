d6f5be4cb17183b0d44e2b89eb33d23790d7227b
Jeremy <jeremy@edos.io>
2015-05-03 15:23:54 -0600

    failtouninstallnoprotectiondangerous script final

--- resources/scripts/post-commit-process.sh
+++ resources/scripts/post-commit-process.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-chgrp
-cd /var/www/hatches-api/
-# probably not needed
-#npm install -g n
-#npm n stable
 apt-get update
 apt-get install php5-curl -y
 apt-get install php5-gd -y
+touch hatches-api/storage/database.sqlite
+chgrp -R www-data hatches-api/
+cd /var/www/hatches-api/
 composer self-update
 composer install --prefer-source --no-dev
 chgrp -R www-data /var/www/hatches-api/
@@ -14,7 +12,6 @@ rm /etc/nginx/sites-enabled/default
 cp nginx.default /etc/nginx/sites-available/default
 service nginx restart;
 mv .env.blank .env
-touch storage/database.sqlite
 chmod -R 0775 /var/www/hatches-api/storage && chmod 0777 storage/database.sqlite
 php artisan key:generate
 php artisan flyfish
