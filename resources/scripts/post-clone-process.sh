6c119787d51df80a0b8673845d3205a24221accc
Jeremy <jeremy@edos.io>
2015-05-03 08:49:15 -0600

    fix ridiculous over-tired-authored sort of shell script. or something.

--- resources/scripts/post-clone-process.sh
+++ resources/scripts/post-clone-process.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
+
+sudo -i
+npm install -g n
+npm n stable
+cd /var/www
+git clone https://github.com/defenestrator/hatches-api.git
+cd hatches-api/
+composer self-update
+composer install --prefer-source
+npm install
 touch /var/www/hatches-api/storage/database.sqlite /var/www/hatches-api/storage/acceptance/acceptance.sqlite
 chmod -R 0775 /var/www/hatches-api/storage && chmod 0777 /var/www/hatches-api/storage/database.sqlite /var/www/hatches-api/storage/acceptance/acceptance.sqlite
 npm install -g gulp
 php artisan flyfish
 php artisan key:generate
-chgrp -R www-data /var/www/hatches-api;
-chmod -R 0777 /var/www/hatches-api/storage;
 php artisan migrate --seed
 php artisan vendor:publish
 behat --init
