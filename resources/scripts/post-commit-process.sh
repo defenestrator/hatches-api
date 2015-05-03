5abdaf7d06a6a70740961d30d7b5fca159fd10c1
Jeremy <jeremy@edos.io>
2015-05-03 10:55:51 -0600

    prefer source

--- resources/scripts/post-commit-process.sh
+++ resources/scripts/post-commit-process.sh
@@ -4,7 +4,7 @@ cd /var/www/hatches-api/
 #npm install -g n
 #npm n stable
 composer self-update
-composer install --prefer-dist
+composer install --prefer-source
 npm install
 chmod chgroup -R www-data /var/www/hatches-api/
 # I think our fresh server image probably has this config, but we need to update it from the confluence doc date:05/03/2015.
