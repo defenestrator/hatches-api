aac50c07e45dfc824493222fd1b4e2263231d9f0
Jeremy <jeremy@edos.io>
2015-05-03 15:47:43 -0600

    death to fanatics

--- resources/scripts/post-commit-process.sh
+++ resources/scripts/post-commit-process.sh
@@ -7,12 +7,11 @@ chgrp -R www-data hatches-api/
 cd /var/www/hatches-api/
 composer self-update
 composer install --prefer-source --no-dev
-chgrp -R www-data /var/www/hatches-api/
-rm /etc/nginx/sites-enabled/default
+rm /etc/nginx/sites-available/default
 cp nginx.default /etc/nginx/sites-available/default
 service nginx restart;
 mv .env.blank .env
-chmod -R 0775 /var/www/hatches-api/storage && chmod 0777 storage/database.sqlite
+chmod -R 0777 /var/www/hatches-api/storage
 php artisan key:generate
 php artisan flyfish
 php artisan migrate --seed --force #will be removed in realsies production env
