a85472166b97efc6a039aaf2521b93510f259eb3
Jeremy <jeremy@edos.io>
2015-05-03 11:24:53 -0600

    stuff

--- resources/scripts/post-commit-process.sh
+++ resources/scripts/post-commit-process.sh
@@ -6,11 +6,11 @@ cd /var/www/hatches-api/
 composer self-update
 composer install --prefer-source
 npm install
-chmod chgrp -R www-data /var/www/hatches-api/
+chgrp -R www-data /var/www/hatches-api/
 # I think our fresh server image probably has this config, but we need to update it from the confluence doc date:05/03/2015.
-# rm /etc/nginx/sites-available/default;
-# mv nginx.default /etc/nginx/sites-available/default;
-service nginx restart;
+rm /etc/nginx/sites-available/default;
+mv nginx.default /etc/nginx/sites-available/default;
+# service nginx restart;
 mv .env.behat.travis .env.behat && mv .env.blank .env
 touch /var/www/hatches-api/storage/database.sqlite /var/www/hatches-api/storage/acceptance/acceptance.sqlite
 chmod -R 0775 /var/www/hatches-api/storage && chmod 0777 /var/www/hatches-api/storage/database.sqlite /var/www/hatches-api/storage/acceptance/acceptance.sqlite
