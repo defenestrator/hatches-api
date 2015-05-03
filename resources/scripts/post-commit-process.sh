357bb458a180c816445c6b54c2bb4a089c86822d
Jeremy <jeremy@edos.io>
2015-05-03 14:32:12 -0600

    failtouninstallnoprotectiondangerous script final

--- resources/scripts/post-commit-process.sh
+++ resources/scripts/post-commit-process.sh
@@ -7,19 +7,16 @@ apt-get update
 apt-get install php5-curl -y
 apt-get install php5-gd -y
 composer self-update
-composer install --prefer-source
-npm install
+composer install --prefer-source --no-dev
 chgrp -R www-data /var/www/hatches-api/hatches-api/
-rm /etc/nginx/sites-enabled/default
-cp nginx.default /etc/nginx/sites-available/default
+#rm /etc/nginx/sites-enabled/default
+#cp nginx.default /etc/nginx/sites-available/default
 service nginx restart;
-mv .env.behat.travis .env.behat && mv .env.blank .env
-touch storage/database.sqlite storage/acceptance/acceptance.sqlite
-chmod -R 0775 /var/www/hatches-api/storage && chmod 0777 storage/database.sqlite storage/acceptance/acceptance.sqlite
+mv .env.blank .env
+touch storage/database.sqlite
+chmod -R 0775 /var/www/hatches-api/storage && chmod 0777 storage/database.sqlite
 npm install -g gulp
 php artisan key:generate
 php artisan flyfish
 php artisan migrate --seed --force #will be removed in realsies production env
 php artisan vendor:publish
-php vendor/bin/behat --init
-gulp test
