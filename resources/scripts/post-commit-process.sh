033fec5f47a9fca56c7fd13bb47a16d9c423d088
Jeremy <jeremy@edos.io>
2015-05-03 10:16:44 -0600

    forgot chgroup!

--- resources/scripts/post-commit-process.sh
+++ resources/scripts/post-commit-process.sh
@@ -7,10 +7,11 @@ cd /var/www/hatches-api/
 composer self-update
 composer install --prefer-source
 npm install
+chmod chgroup -R www-data /var/www/hatches-api/
 # I think our fresh server image probably has this config, but we need to update it from the confluence doc date:05/03/2015.
 # rm /etc/nginx/sites-available/default;
 # mv nginx.default /etc/nginx/sites-available/default;
-# service nginx restart;
+service nginx restart;
 mv .env.example .env && mv .env.behat.travis .env.behat
 touch /var/www/hatches-api/storage/database.sqlite /var/www/hatches-api/storage/acceptance/acceptance.sqlite
 chmod -R 0775 /var/www/hatches-api/storage && chmod 0777 /var/www/hatches-api/storage/database.sqlite /var/www/hatches-api/storage/acceptance/acceptance.sqlite
