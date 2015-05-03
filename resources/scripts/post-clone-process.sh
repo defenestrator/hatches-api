ce609eb641635dc149c46e3be7913c44543df014
Jeremy <jeremy@edos.io>
2015-05-03 08:43:03 -0600

    fix ridiculous over-tired-authored sort of shell script.

--- resources/scripts/post-clone-process.sh
+++ resources/scripts/post-clone-process.sh
@@ -1,14 +1,12 @@
-#! /bin/bash
-mv .env.example .env;
-mv .env.behat.travis .env.behat;
-php artisan key:generate;
-php artisan vendor:publish;
-rm /etc/nginx/sites-available/default;
-mv nginx.default /etc/nginx/sites-available/default;
-service nginx restart;
-touch /var/www/hatches-api/storage/database.sqlite /var/www/hatches-api/storage/acceptance/acceptance.sqlite;
+#!/bin/bash
+touch /var/www/hatches-api/storage/database.sqlite /var/www/hatches-api/storage/acceptance/acceptance.sqlite
+chmod -R 0775 /var/www/hatches-api/storage && chmod 0777 /var/www/hatches-api/storage/database.sqlite /var/www/hatches-api/storage/acceptance/acceptance.sqlite
+npm install -g gulp
+php artisan flyfish
+php artisan key:generate
 chgrp -R www-data /var/www/hatches-api;
 chmod -R 0777 /var/www/hatches-api/storage;
-php artisan migrate --seed --force;
-vendor/bin/behat --init;
-gulp test;
+php artisan migrate --seed
+php artisan vendor:publish
+behat --init
+gulp test
