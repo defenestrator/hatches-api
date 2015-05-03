ffdcdc7ff6e3be19bca7705d6d1df64fc57008aa
Jeremy <jeremy@edos.io>
2015-05-03 09:17:22 -0600

    rollback fix phpunit config

--- resources/scripts/post-commit-process.sh
+++ resources/scripts/post-commit-process.sh
@@ -6,9 +6,10 @@ npm n stable
 composer self-update
 composer install --prefer-source
 npm install
-rm /etc/nginx/sites-available/default;
-mv nginx.default /etc/nginx/sites-available/default;
-service nginx restart;
+# fresh server image, it probably has this config.
+# rm /etc/nginx/sites-available/default;
+# mv nginx.default /etc/nginx/sites-available/default;
+# service nginx restart;
 mv .env.example .env && mv .env.behat.travis .env.behat;
 touch /var/www/hatches-api/storage/database.sqlite /var/www/hatches-api/storage/acceptance/acceptance.sqlite
 chmod -R 0775 /var/www/hatches-api/storage && chmod 0777 /var/www/hatches-api/storage/database.sqlite /var/www/hatches-api/storage/acceptance/acceptance.sqlite
