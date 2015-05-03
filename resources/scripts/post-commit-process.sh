a611fce42747cb2b35f555057ecda629de244018
Jeremy <jeremy@edos.io>
2015-05-03 12:23:27 -0600

    nginx

--- resources/scripts/post-commit-process.sh
+++ resources/scripts/post-commit-process.sh
@@ -9,18 +9,18 @@ apt-get install php5-gd
 composer self-update
 composer install --prefer-source
 npm install
-chgrp -R www-data /var/www/hatches-api/
+chgrp -R www-data ./hatches-api/
 # I think our fresh server image probably has this config, but we need to update it from the confluence doc date:05/03/2015.
 rm /etc/nginx/sites-available/default;
-mv nginx.default /etc/nginx/sites-available/default;
+mv ./nginx.default /etc/nginx/sites-available/default;
 service nginx restart;
 mv .env.behat.travis .env.behat && mv .env.blank .env
-touch /var/www/hatches-api/storage/database.sqlite /var/www/hatches-api/storage/acceptance/acceptance.sqlite
-chmod -R 0775 /var/www/hatches-api/storage && chmod 0777 /var/www/hatches-api/storage/database.sqlite /var/www/hatches-api/storage/acceptance/acceptance.sqlite
+touch ./storage/database.sqlite ./storage/acceptance/acceptance.sqlite
+chmod -R 0775 /var/www/hatches-api/storage && chmod 0777 ./storage/database.sqlite ./storage/acceptance/acceptance.sqlite
 npm install -g gulp
 php artisan key:generate
 php artisan flyfish
 php artisan migrate --seed --force #will be remved in realsies production env
 php artisan vendor:publish
-./var/www/hatches-api/vendor/bin/behat --init
+php ./vendor/bin/behat --init
 gulp test
