5cf382e37dcb20c65e3f6dd113121c8fd9fa7027
Jeremy <jeremy@edos.io>
2015-05-03 13:28:56 -0600

    ninstall script final

--- resources/scripts/post-commit-process.sh
+++ resources/scripts/post-commit-process.sh
@@ -4,23 +4,23 @@ cd /var/www/hatches-api/
 #npm install -g n
 #npm n stable
 apt-get update
-apt-get install php5-curl
-apt-get install php5-gd
+apt-get install php5-curl -y
+apt-get install php5-gd -y
 composer self-update
 composer install --prefer-source
 npm install
-chgrp -R www-data ./hatches-api/
+chgrp -R www-data /var/www/hatches-api/hatches-api/
 # I think our fresh server image probably has this config, but we need to update it from the confluence doc date:05/03/2015.
 rm /etc/nginx/sites-available/default
-mv ./nginx.default /etc/nginx/sites-available/default
+mv nginx.default /etc/nginx/sites-available/default
 service nginx restart;
 mv .env.behat.travis .env.behat && mv .env.blank .env
-touch ./storage/database.sqlite ./storage/acceptance/acceptance.sqlite
-chmod -R 0775 /var/www/hatches-api/storage && chmod 0777 ./storage/database.sqlite ./storage/acceptance/acceptance.sqlite
+touch storage/database.sqlite storage/acceptance/acceptance.sqlite
+chmod -R 0775 /var/www/hatches-api/storage && chmod 0777 storage/database.sqlite storage/acceptance/acceptance.sqlite
 npm install -g gulp
 php artisan key:generate
 php artisan flyfish
 php artisan migrate --seed --force #will be remved in realsies production env
 php artisan vendor:publish
-php ./vendor/bin/behat --init
+php vendor/bin/behat --init
 gulp test
