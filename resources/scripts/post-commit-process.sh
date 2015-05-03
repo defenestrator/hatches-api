9e079e40e773a47d2f160960d9a932e48833b1ca
Jeremy <jeremy@edos.io>
2015-05-03 09:27:14 -0600

    tweaks

--- resources/scripts/post-commit-process.sh
+++ resources/scripts/post-commit-process.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-
+# word, to your mother, even.
 cd /var/www/hatches-api/
 npm install -g n
 npm n stable
 composer self-update
 composer install --prefer-source
 npm install
-# fresh server image, it probably has this config.
+# I think our fresh server image probably has this config, but we need to update it from the confluence doc date:05/03/2015.
 # rm /etc/nginx/sites-available/default;
 # mv nginx.default /etc/nginx/sites-available/default;
 # service nginx restart;
