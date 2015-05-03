3001afe193ccbe74830a0b65bb95108850a54155
Jeremy <jeremy@edos.io>
2015-05-03 01:58:33 -0600

    a nice brittle shell script

--- resources/scripts/post-clone-process.sh
+++ resources/scripts/post-clone-process.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 php artisan key:generate;
-php artisan migrate --seed --force;
 php artisan vendor:publish;
 rm /etc/nginx/sites-available/default;
 mv nginx.default /etc/nginx/sites-available/default;
@@ -8,6 +7,7 @@ service nginx restart;
 touch /var/www/hatches-api/storage/database.sqlite /var/www/hatches-api/storage/acceptance/acceptance.sqlite;
 chgrp -R www-data /var/www/hatches-api;
 chmod -R 0777 /var/www/hatches-api/storage;
+php artisan migrate --seed --force;
 mv .env.example .env && mv .env.behat.travis .env.behat;
 vendor/bin/behat --init;
 gulp test;
