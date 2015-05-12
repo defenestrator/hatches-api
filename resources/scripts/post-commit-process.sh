2de32d0ecff382715b0100aef74a3342792269d8
Jeremy <jeremy@edos.io>
2015-05-12 09:19:05 -0600

    AWS why u no deploy?

--- resources/scripts/post-commit-process.sh
+++ resources/scripts/post-commit-process.sh
@@ -5,14 +5,13 @@ apt-get install php5-gd -y
 touch hatches-api/storage/database.sqlite
 chgrp -R www-data hatches-api/
 cd /var/www/hatches-api/
+chmod -R 0777 /var/www/hatches-api/storage
 composer self-update
 composer install --prefer-source --no-dev --no-progress
 rm /etc/nginx/sites-enabled/default
 cp server.conf /etc/nginx/sites-enabled/default
 service nginx restart
 mv .env.blank .env
-chmod -R 0777 /var/www/hatches-api/storage
 php artisan key:generate
-php artisan flyfish
 php artisan migrate --seed --force #will be removed in realsies production env
 php artisan vendor:publish
