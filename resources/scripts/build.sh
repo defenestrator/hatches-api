b4a689e91ee07a458785187f0fb50dbc1be0dcd6
Jeremy <jeremy@edos.io>
2015-05-14 13:55:36 -0600

    build script

--- resources/scripts/build.sh
+++ resources/scripts/build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+#!/bin/bash
+cd /var/www
+if hatches-api; then
+    rm -rf hatches-api/
+fi
 apt-get update
 apt-get install php5-curl -y
 apt-get install php5-gd -y
-touch hatches-api/storage/database.sqlite
 chgrp -R www-data hatches-api/
 cd /var/www/hatches-api/
 composer self-update
@@ -12,5 +16,5 @@ service nginx restart
 chmod -R 0777 /var/www/hatches-api/storage
 php artisan key:generate
 php artisan flyfish
-php artisan migrate  #will be removed in realsies production env
 php artisan vendor:publish
+php artisan migrate  #will be removed in realsies production env
