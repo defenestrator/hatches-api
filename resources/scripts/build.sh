8c77cd9c7655bbc23a19f65630549392a7585780
Jeremy <jeremy@edos.io>
2015-05-14 17:47:30 -0600

    build script

--- resources/scripts/build.sh
+++ resources/scripts/build.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
+cd /var/www
+rm -rf hatches-api/
 apt-get update
 apt-get install php5-curl -y
 apt-get install php5-gd -y
 apt-get install php5-mysql
+git clone https://github.com/defenestrator/hatches-api.git
+mv hatches.prod.env /var/www/hatches-api/.env
 chgrp -R www-data hatches-api/
 cd /var/www/hatches-api/
+chmod -R 0777 storage/
 composer self-update
-mv /var/hatches.prod.env /var/www/hatches-api/.env
 composer install --prefer-source --no-dev --no-progress
-chmod -R 0777 /var/www/hatches-api/storage
 php artisan key:generate
 php artisan flyfish
 php artisan vendor:publish
-php artisan migrate  --force #will be removed in realsies production env
+php artisan migrate  --force
