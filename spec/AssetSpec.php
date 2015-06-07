844e49d764ffd5b9985bdea59efd7b6369226c45
Jeremy <jeremy@edos.io>
2015-06-06 21:25:53 -0600

    test visualizers, not sure what else.

--- spec/AssetSpec.php
+++ spec/AssetSpec.php
@@ -5,11 +5,17 @@ namespace spec\Hatches;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
+
 class AssetSpec extends ObjectBehavior
 {
     function it_is_initializable()
     {
         $this->shouldHaveType('Hatches\Asset');
+    }
+
+    function it_stores_a_path_to_a_media_asset()
+    {
+        
     }
 
 }
